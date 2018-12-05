f3c0621bd77c85fb3a77478aa5b14aa329e9eb10
GitHub <noreply@github.com>
2018-12-05 12:15:12 +0000

    1bfff59 chore/separate edit and parse functionality from React (#366)

--- editor/Editor.js
+++ editor/Editor.js
@@ -1,5 +1,3 @@
-import _extends from 'babel-runtime/helpers/extends';
-import _defineProperty from 'babel-runtime/helpers/defineProperty';
 import _Object$getPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of';
 import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
 import _createClass from 'babel-runtime/helpers/createClass';
@@ -7,85 +5,25 @@ import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructo
 import _inherits from 'babel-runtime/helpers/inherits';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import convertCtrlKey from './convertCtrlKey';
 
 var Editor = function (_Component) {
     _inherits(Editor, _Component);
 
-    function Editor(props) {
+    function Editor() {
+        var _ref;
+
+        var _temp, _this, _ret;
+
         _classCallCheck(this, Editor);
 
-        var _this = _possibleConstructorReturn(this, (Editor.__proto__ || _Object$getPrototypeOf(Editor)).call(this, props));
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
 
-        _this.onKeyDown = function (event) {
-            var key = event.key,
-                ctrlKey = event.ctrlKey,
-                metaKey = event.metaKey;
-
-            var element = event.target;
-
-            _this.setState({ element: element });
-
-            // bold
-            if (key === 'b' && (ctrlKey || metaKey)) {
-                _this.insertMarkers('bold');
-                // italic
-            } else if (key === 'i' && (ctrlKey || metaKey)) {
-                _this.insertMarkers('italic');
-            }
-        };
-
-        _this.toggleMode = function (mode) {
-            var prop = mode + 'Mode';
-
-            _this.setState(_defineProperty({}, prop, !_this.state[prop]));
-        };
-
-        _this.insertMarkers = function (mode) {
-            var element = _this.state.element;
-            var selectionStart = element.selectionStart,
-                selectionEnd = element.selectionEnd,
-                value = element.value;
-
-
-            _this.toggleMode(mode);
-
-            var marker = void 0;
-
-            switch (mode) {
-                case 'italic':
-                    marker = '_';
-                    break;
-                case 'bold':
-                    marker = '*';
-                    break;
-                default:
-                    return;
-            }
-
-            var newValue = void 0;
-
-            if (selectionStart >= 0 && selectionStart === selectionEnd) {
-                newValue = '' + value + marker;
-            } else if (selectionStart >= 0) {
-                newValue = [value.slice(0, selectionStart), value.slice(selectionStart, selectionEnd), value.slice(selectionEnd)].join(marker);
-
-                _this.toggleMode(mode);
-            }
-
-            if (_this.props.onEdit) {
-                _this.props.onEdit(newValue);
-            }
-        };
-
-        _this.defaultState = {
-            boldMode: false,
-            italicMode: false
-        };
-
-        _this.state = _extends({}, _this.defaultState, {
-            element: null
-        });
-        return _this;
+        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Editor.__proto__ || _Object$getPrototypeOf(Editor)).call.apply(_ref, [this].concat(args))), _this), _this.onKeyDown = function (event) {
+            convertCtrlKey(event, _this.props.onEdit);
+        }, _temp), _possibleConstructorReturn(_this, _ret);
     }
 
     _createClass(Editor, [{
@@ -110,7 +48,8 @@ Editor.defaultProps = {
 };
 
 Editor.propTypes = {
-    onEdit: PropTypes.func
+    onEdit: PropTypes.func,
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
 };
 
 export default Editor;
--- parser/Parser.js
+++ parser/Parser.js
@@ -5,102 +5,7 @@ import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructo
 import _inherits from 'babel-runtime/helpers/inherits';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-import MarkdownIt from 'markdown-it';
-
-var emojiDb = {
-    ':-)': '\uD83D\uDE42',
-    ':)': '\uD83D\uDE42',
-    ':-(': '\uD83D\uDE41',
-    ':(': '\uD83D\uDE41',
-    ':+1': '\uD83D\uDC4D',
-    ':-1': '\uD83D\uDC4E'
-};
-
-var codes = {
-    bold: {
-        name: 'bold',
-        char: '*',
-        domEl: 'strong',
-        encodedChar: 0x2a,
-        regexString: '^\\*((?!\\s)[^*]+(?:[^\\s]))\\*',
-        contentFn: function contentFn(val) {
-            return val;
-        }
-    },
-    italic: {
-        name: 'italic',
-        char: '_',
-        domEl: 'em',
-        encodedChar: 0x5f,
-        regexString: '^_((?!\\s)[^_]+(?:[^\\s]))_',
-        contentFn: function contentFn(val) {
-            return val;
-        }
-    },
-    emoji: {
-        name: 'emoji',
-        char: ':',
-        domEl: 'span',
-        encodedChar: 0x3a,
-        regexString: '^(:-\\)|:\\)|:\\(|:-\\(|:\\+1|:-1)',
-        contentFn: function contentFn(val) {
-            return emojiDb[val];
-        }
-    }
-};
-
-var parse = function parse(code) {
-    return function (state, silent) {
-        if (silent) return false;
-
-        var start = state.pos;
-        var marker = state.src.charCodeAt(start);
-
-        // marker character: "_", "*", ":"
-        if (marker !== codes[code].encodedChar) {
-            return false;
-        }
-
-        var MARKER_REGEX = new RegExp(codes[code].regexString);
-        var token = state.src.slice(start);
-
-        if (MARKER_REGEX.test(token)) {
-            var markerMatch = token.match(MARKER_REGEX);
-            var text = markerMatch[1];
-
-            state.push(codes[code].domEl + '_open', codes[code].domEl, 1);
-
-            var t = state.push('text', '', 0);
-            t.content = codes[code].contentFn(text);
-
-            state.push(codes.bold.domEl + '_close', codes[code].domEl, -1);
-            state.pos += markerMatch[0].length;
-
-            return true;
-        }
-
-        return false;
-    };
-};
-
-var initParser = function initParser() {
-    // disable all rules, enable autolink for URLs and email addresses
-    var md = new MarkdownIt('zero', { linkify: true });
-
-    // *bold* -> <strong>bold</strong>
-    md.inline.ruler.push('strong', parse(codes.bold.name));
-
-    // _italic_ -> <em>italic</em>
-    md.inline.ruler.push('italic', parse(codes.italic.name));
-
-    // :-) :) :-( :( :+1 :-1 -> <span>[unicode]</span>
-    md.inline.ruler.push('emoji', parse(codes.emoji.name));
-
-    md.enable(['linkify', 'strong', 'italic', 'emoji']);
-
-    return md;
-};
+import MdParserClass from './MdParser';
 
 var Parser = function (_Component) {
     _inherits(Parser, _Component);
@@ -110,7 +15,7 @@ var Parser = function (_Component) {
 
         var _this = _possibleConstructorReturn(this, (Parser.__proto__ || _Object$getPrototypeOf(Parser)).call(this, props));
 
-        _this.parser = initParser();
+        _this.MdParser = new MdParserClass();
         return _this;
     }
 
@@ -125,7 +30,7 @@ var Parser = function (_Component) {
             return React.createElement('p', {
                 style: style,
                 dangerouslySetInnerHTML: {
-                    __html: this.parser.renderInline(children)
+                    __html: this.MdParser.render(children)
                 }
             });
         }
@@ -139,7 +44,8 @@ Parser.defaultProps = {
 };
 
 Parser.propTypes = {
-    style: PropTypes.object
+    style: PropTypes.object,
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
 };
 
 export default Parser;

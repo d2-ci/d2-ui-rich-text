2dbfbe6fd951c9557a64e551803b787079c05ced
joakimia <joakimamundsen91@gmail.com>
2019-01-16 03:59:48 +0000

    e6d83dc add update of sharing in model -> interpretation if manage sharing link is clicked during edit of existing interpretation

---
+++ editor/convertCtrlKey.js
@@ -0,0 +1,61 @@
+var state = {
+    boldMode: false,
+    italicMode: false,
+    element: null
+};
+
+var markerMap = {
+    italic: '_',
+    bold: '*'
+};
+
+var toggleMode = function toggleMode(mode) {
+    var prop = mode + 'Mode';
+
+    state[prop] = !state[prop];
+};
+
+var insertMarkers = function insertMarkers(mode, cb) {
+    toggleMode(mode);
+
+    var marker = markerMap[mode] || null;
+    if (!marker || !cb) {
+        return;
+    }
+
+    var element = state.element;
+    var selectionStart = element.selectionStart,
+        selectionEnd = element.selectionEnd,
+        value = element.value;
+
+    var newValue = void 0;
+
+    if (selectionStart >= 0 && selectionStart === selectionEnd) {
+        newValue = '' + value + marker;
+    } else if (selectionStart >= 0) {
+        newValue = [value.slice(0, selectionStart), value.slice(selectionStart, selectionEnd), value.slice(selectionEnd)].join(marker);
+
+        toggleMode(mode);
+    }
+
+    cb(newValue);
+};
+
+var convertCtrlKey = function convertCtrlKey(event, cb) {
+    var key = event.key,
+        ctrlKey = event.ctrlKey,
+        metaKey = event.metaKey;
+
+
+    var element = event.target;
+
+    state.element = element;
+
+    if (key === "b" && (ctrlKey || metaKey)) {
+        insertMarkers("bold", cb);
+    } else if (key === "i" && (ctrlKey || metaKey)) {
+        insertMarkers("italic", cb);
+    }
+};
+
+export default convertCtrlKey;

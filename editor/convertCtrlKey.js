ea3789991e8da6427e166947cb647eaa9043e945
Jen Jones Arnesen <jennifer@dhis2.org>
2019-01-24 17:37:44 +0000

    f84c5ec trim whitespaces from selection

--- editor/convertCtrlKey.js
+++ editor/convertCtrlKey.js
@@ -9,6 +9,13 @@ var markerMap = {
     bold: '*'
 };
 
+var trim = function trim(str) {
+    var leftSpaces = /^\s+/;
+    var rightSpaces = /\s+$/;
+
+    return str.replace(leftSpaces, '').replace(rightSpaces, '');
+};
+
 var toggleMode = function toggleMode(mode) {
     var prop = mode + 'Mode';
 
@@ -16,31 +23,53 @@ var toggleMode = function toggleMode(mode) {
 };
 
 var insertMarkers = function insertMarkers(mode, cb) {
-    toggleMode(mode);
+    var _state$element = state.element,
+        start = _state$element.selectionStart,
+        end = _state$element.selectionEnd,
+        value = _state$element.value;
 
     var marker = markerMap[mode] || null;
-    if (!marker || !cb) {
+    if (!marker || !cb || start < 0) {
         return;
     }
 
-    var element = state.element;
-    var selectionStart = element.selectionStart,
-        selectionEnd = element.selectionEnd,
-        value = element.value;
+    toggleMode(mode);
 
     var newValue = void 0;
+    var caretPos = end + 1;
 
-    if (selectionStart >= 0 && selectionStart === selectionEnd) {
+    var padMarkers = function padMarkers(text) {
+        if (value.length && start > 0 && value[start - 1] !== ' ') {
+            text = ' ' + text;
+            ++caretPos;
+        }
+
+        if (value.length && end !== value.length && value[end] !== ' ') {
+            text = text + ' ';
+        }
+
+        return text;
+    };
+
+    if (start === end) {
+        //no text
         var valueArr = value.split('');
-        valueArr.splice(selectionStart, 0, marker);
+
+        valueArr.splice(start, 0, padMarkers('' + marker + marker));
         newValue = valueArr.join('');
-    } else if (selectionStart >= 0) {
-        newValue = [value.slice(0, selectionStart), value.slice(selectionStart, selectionEnd), value.slice(selectionEnd)].join(marker);
+    } else {
+        var text = value.slice(start, end);
+        var trimmedText = trim(text);
+
+        // adjust caretPos based on trimmed text selection
+        caretPos = caretPos - (text.length - trimmedText.length) + 1;
+
+        newValue = [value.slice(0, start), padMarkers('' + marker + trimmedText + marker), value.slice(end)].join('');
 
         toggleMode(mode);
     }
 
-    cb(newValue);
+    cb(newValue, caretPos);
 };
 
 var convertCtrlKey = function convertCtrlKey(event, cb) {

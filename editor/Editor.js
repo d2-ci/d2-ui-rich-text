2317e21cb633d4cec769693ee0469638b23db288
Jen Jones Arnesen <jennifer@dhis2.org>
2019-04-09 20:11:15 +0000

    787169d use @dhis2/d2-ui-analytics

--- editor/Editor.js
+++ editor/Editor.js
@@ -1,55 +1,85 @@
-import _Object$getPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of';
-import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
-import _createClass from 'babel-runtime/helpers/createClass';
-import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
-import _inherits from 'babel-runtime/helpers/inherits';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import convertCtrlKey from './convertCtrlKey';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _convertCtrlKey = require('./convertCtrlKey');
+
+var _convertCtrlKey2 = _interopRequireDefault(_convertCtrlKey);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Editor = function (_Component) {
-    _inherits(Editor, _Component);
+    (0, _inherits3.default)(Editor, _Component);
 
     function Editor() {
         var _ref;
 
         var _temp, _this, _ret;
 
-        _classCallCheck(this, Editor);
+        (0, _classCallCheck3.default)(this, Editor);
 
         for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
             args[_key] = arguments[_key];
         }
 
-        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Editor.__proto__ || _Object$getPrototypeOf(Editor)).call.apply(_ref, [this].concat(args))), _this), _this.onKeyDown = function (event) {
-            convertCtrlKey(event, _this.props.onEdit);
-        }, _temp), _possibleConstructorReturn(_this, _ret);
+        return _ret = (_temp = (_this = (0, _possibleConstructorReturn3.default)(this, (_ref = Editor.__proto__ || (0, _getPrototypeOf2.default)(Editor)).call.apply(_ref, [this].concat(args))), _this), _this.onKeyDown = function (event) {
+            (0, _convertCtrlKey2.default)(event, _this.props.onEdit);
+        }, _temp), (0, _possibleConstructorReturn3.default)(_this, _ret);
     }
 
-    _createClass(Editor, [{
+    (0, _createClass3.default)(Editor, [{
         key: 'render',
         value: function render() {
             var children = this.props.children;
 
 
-            return React.createElement(
+            return _react2.default.createElement(
                 'div',
                 { onKeyDown: this.onKeyDown },
                 children
             );
         }
     }]);
-
     return Editor;
-}(Component);
+}(_react.Component);
 
 Editor.defaultProps = {
     onEdit: null
 };
 
 Editor.propTypes = {
-    onEdit: PropTypes.func,
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    onEdit: _propTypes2.default.func,
+    children: _propTypes2.default.oneOfType([_propTypes2.default.arrayOf(_propTypes2.default.node), _propTypes2.default.node])
 };
 
-export default Editor;
+exports.default = Editor;

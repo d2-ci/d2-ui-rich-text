b9f16631d5d3720838dcc434eb737ab77f86e5ff
GitHub <noreply@github.com>
2019-08-19 07:39:11 +0000

    10c0e5e fix: fix rich text regexp to allow non-word characters after bold/italic (#443)

--- parser/MdParser.js
+++ parser/MdParser.js
@@ -33,8 +33,8 @@ var codes = {
         char: "*",
         domEl: "strong",
         encodedChar: 0x2a,
-        // see https://regex101.com/r/evswdV/1 for explanation of regexp
-        regexString: "(?:(?!\\S).|^)\\*((?!\\s)[^*]+(?:[^\\s]))\\*(?!\\S)",
+        // see https://regex101.com/r/evswdV/6 for explanation of regexp
+        regexString: "\\B\\*((?!\\s)[^*]+)\\b\\*\\B",
         contentFn: function contentFn(val) {
             return val;
         }
@@ -44,8 +44,8 @@ var codes = {
         char: "_",
         domEl: "em",
         encodedChar: 0x5f,
-        // see https://regex101.com/r/evswdV/1 for explanation of regexp
-        regexString: "(?:(?!\\S).|^)_((?!\\s)[^_]+(?:[^\\s]))_(?!\\S)",
+        // see https://regex101.com/r/p6LpjK/5 for explanation of regexp
+        regexString: "\\b_((?!\\s)[^_]+)\\B_\\b",
         contentFn: function contentFn(val) {
             return val;
         }
